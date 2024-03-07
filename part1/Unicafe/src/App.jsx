c8aec63d0e4001137ef3c4c2c9818a53dec1d5d8
Alex Scheick <141498430+ASCAlex@users.noreply.github.com>
2024-03-07 08:53:27 +0100

    exercise 1.7 done

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css';
 
 const Header = ({text}) => (<h1>{text}</h1>)
 
@@ -6,19 +7,16 @@ const Button = ({onClick, text}) => (
     <button onClick={onClick}>{text}</button>
 )
 
-const Stats = ({text, count}) => (
-    <p>
-        {text[0]} {count[0]} <br/>
-        {text[1]} {count[1]} <br/>
-        {text[2]} {count[2]}
-    </p>
-)
+const Text = ({text}) => <p>{text}</p>
 
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const all = good + neutral + bad
+    const average = all !== 0 ? (good - bad) / (good + neutral + bad) : 0
+    const positive = good !== 0 ? good / all * 100 : 0
 
     return (
         <div>
@@ -29,7 +27,12 @@ const App = () => {
             <Button onClick={() => setBad(bad + 1)} text={"bad"} />
 
             <Header text={"statistics"} />
-            <Stats text={["good", "neutral", "bad"]} count={[good, neutral, bad]} />
+            <Text text={"good " + good} />
+            <Text text={"neutral " + neutral} />
+            <Text text={"bad " + bad} />
+            <Text text={"all " + all} />
+            <Text text={"average " + average} />
+            <Text text={"positive " + positive + " %"} />
 
 
         </div>
