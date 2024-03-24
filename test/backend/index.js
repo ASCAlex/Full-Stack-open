803f24e20d027eabee82f597971b90526ffeb6a5
Alex Scheick <141498430+ASCAlex@users.noreply.github.com>
2024-03-24 09:04:08 +0100

    implemented HTTP Request to update DB implemented express error handler

--- test/backend/index.js
+++ test/backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const Note = require('./models/note')
+
 
 app.use(cors())
 app.use(express.json())
@@ -49,23 +51,23 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id).then(note => {
+        if (note) {
+            response.json(note)
+            mongoose.connection.close()
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const generateID = () => {
@@ -76,24 +78,36 @@ const generateID = () => {
 }
 
 app.post('/api/notes', (request, response) => {
-    // if content is empty don't add the note
     const body = request.body
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
+
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateID()
-    }
+        important: body.important || false,
+    })
 
-    notes = notes.concat(note)
-    console.log(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+        mongoose.connection.close()
+    })
+})
 
-    response.json(note)
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+
+    const note = new Note( {
+        content: body.content,
+        important: body.important
+    })
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -103,7 +117,20 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 // choose environment variable or if undefined 3001 as PORT
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
