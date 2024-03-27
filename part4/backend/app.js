a5873b95b733ff3c3e200ffcac2b829949bb9611
Alex Scheick <141498430+ASCAlex@users.noreply.github.com>
2024-03-27 16:13:57 +0100

    exercise 4.1 and 4.2 done

---
+++ part4/backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.set('strictQuery', false)
+
+logger.info('Connecting to MongoDB')
+
+mongoose.connect(config.MONGODB_URI)
+
+logger.info('Connected to MongoDB')
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
