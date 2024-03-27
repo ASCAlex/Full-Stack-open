29d9393d55b2dca041648edaa351a15c44259e26
Alex Scheick <141498430+ASCAlex@users.noreply.github.com>
2024-03-27 08:30:31 +0100

    updated project structure

--- test/backend/models/note.js
+++ test/backend/models/note.js
@@ -1,21 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url)
-
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
 const noteSchema = new mongoose.Schema({
-    content: String,
+    content: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
     important: Boolean,
 })
 
